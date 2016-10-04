a94c9d6d08e8facc3104ed720631288faad649fc
Beccafox123 <rebeccafox12333@gmail.com>
2016-10-03 23:52:45 -0400

    Assignment 2

---
+++ Assignment 2/Assignment 2/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Person Spouse;
+        public static int Count;
+        public static double SumOfAllAges;
+
+        public static double AverageAge()
+        {
+            return Person.SumOfAllAges / Person.Count;
+        }
+
+        public void InfoNeeded()
+        {
+            System.Console.Write("Enter Your First Name: ");
+            FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Last Name: ");
+            LastName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Age: ");
+            Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public void SpouseInfo()
+        {
+            this.Spouse = new Person();
+
+            System.Console.Write("Enter Your Spouse's First Name: ");
+            Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Enter Your Spouse's Age: ");
+            Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += Age;
+        }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public string GetSpouseFullName()
+        {   
+            return Spouse.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(GetFullName() + " " + (Age));
+            System.Console.WriteLine(GetSpouseFullName() + " " + (Spouse.Age));
+        }
+
+
+
+    }
+}
---
+++ Assignment 2/Assignment 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.Count = 0;
+
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            System.Console.WriteLine(" -===+ p1 Information +===- ");
+            System.Console.WriteLine();
+
+            p1.InfoNeeded();
+            p1.SpouseInfo();
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine(" -===+ p2 Information +===- ");
+            System.Console.WriteLine();
+
+            p2.InfoNeeded();
+            p2.SpouseInfo();
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine(" -===+ Summary +===- ");
+            System.Console.WriteLine();
+
+            p1.PrintNameAndAge();
+            p2.PrintNameAndAge();
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("Average Age = " + Person.AverageAge());
+
+            System.Threading.Thread.Sleep(8000);
+        }
+    }
+}
