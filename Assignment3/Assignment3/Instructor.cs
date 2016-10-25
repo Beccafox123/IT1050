9d52a0f40e9f8dd50f786dcf3d68595257a866d1
Beccafox123 <rebeccafox12333@gmail.com>
2016-10-25 16:17:05 -0400

    Assignment 3
    Assignment 3

---
+++ Assignment3/Assignment3/Instructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Instructor
+    {
+        private string Name;
+        private string CourseName;
+
+        public string GetNameAndCourse() { return this.Name + " who teaches " + this.CourseName; }
+
+        public Instructor(string teacherFirstName , string courseName)
+        { this.Name = teacherFirstName; this.CourseName = courseName; }
+
+        public void SetStudentGrade(Student student, int grade) { student.SetGrade(grade); }
+
+        public void PrintNameAndCourse() { System.Console.WriteLine(GetNameAndCourse()); }
+
+    }
+}
---
+++ Assignment3/Assignment3/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public Student() { Grade = 0; }
+        public int GetGrade() { return this.Grade; }
+        public void SetGrade(int x) { this.Grade = x; }
+
+        public Student(string name, Instructor theTeacher) { this.Name = name; this.Teacher = theTeacher; }
+
+
+        public void PrintStudentInfo()
+        { System.Console.WriteLine("Name : " + this.Name + "  Grade: " + this.Grade + "  Teacher: " + Teacher.GetNameAndCourse()); }
+
+        
+
+        
+
+
+
+    }
+}
